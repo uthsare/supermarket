9f9dd3d0dea2d4bc055421002cd540054c2a9646
uthsare <uthsara2k02@gmail.com>
2023-09-12 19:06:33 +0530

    Order

---
+++ Order/Controller/OrderController.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Order.Controller;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import Order.db.DBConnection;
+import Order.Model.OrderDetailModel;
+import Order.Model.OrderModel;
+/**
+ *
+ * @author Uthsara Basnayake
+ */
+public class OrderController {
+    public String placeOrder(OrderModel orderModel, ArrayList<OrderDetailModel> orderDetailModels) throws SQLException {
+        Connection connection = DBConnection.getInstance().getConnection();
+        try {
+            connection.setAutoCommit(false);
+            String orderQuery = "INSERT INTO Orders VALUES(?,?,?)";
+
+            PreparedStatement statementForOrder = connection.prepareStatement(orderQuery);
+            statementForOrder.setString(1, orderModel.getOrderId());
+            statementForOrder.setString(2, orderModel.getOrderDate());
+            statementForOrder.setString(3, orderModel.getCutomerId());
+
+            if (statementForOrder.executeUpdate() > 0) {
+
+                boolean isOrderDetailSaved = true;
+                String orderDetailQuery = "INSERT INTO orderdetail VALUES(?,?,?,?)";
+
+                for (OrderDetailModel orderDetailModel : orderDetailModels) {
+                    PreparedStatement statementForOrderDetail = connection.prepareStatement(orderDetailQuery);
+                    statementForOrderDetail.setString(1, orderModel.getOrderId());
+                    statementForOrderDetail.setString(2, orderDetailModel.getItemCode());
+                    statementForOrderDetail.setInt(3, orderDetailModel.getQty());
+                    statementForOrderDetail.setDouble(4, orderDetailModel.getDiscount());
+                    
+                    if(!(statementForOrderDetail.executeUpdate() > 0)){
+                        isOrderDetailSaved = false;
+                    }
+
+                }
+                
+                if(isOrderDetailSaved){
+                    boolean isItemUpdated = true;
+                    String itemQuery = "UPDATE Item SET QTY = QTY - ? WHERE ID = ?";
+                    for (OrderDetailModel orderDetailModel : orderDetailModels) {
+                        PreparedStatement statmentForItem = connection.prepareStatement(itemQuery);
+                        statmentForItem.setInt(1, orderDetailModel.getQty());
+                        statmentForItem.setString(2, orderDetailModel.getItemCode());
+                        if(!(statmentForItem.executeUpdate()>=0)){
+                            isItemUpdated = false;
+                        }
+                    }
+                    
+                    if(isItemUpdated){
+                        connection.commit();
+                        return "Success";
+                    } else{
+                        connection.rollback();
+                        return "Item Update Error";
+                    }
+                            
+                } else{
+                    connection.rollback();
+                    return "Order Detail Save Error";
+                }
+
+            } else {
+                connection.rollback();
+                return "Order Save Error";
+            }
+
+        } catch (SQLException e) {
+            connection.rollback();
+            return e.getMessage();
+        } finally {
+            connection.setAutoCommit(true);
+        }
+
+    }
+
+}
