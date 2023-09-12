b2bc0e23530d7938eec268c69364457713f5c8a9
uthsare <uthsara2k02@gmail.com>
2023-09-12 15:41:51 +0530

    Initial Commit

---
+++ SuperMarket.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package item;
+import item.view.ItemView;
+import Customer.view.CustomerView;
+/**
+ *
+ * @author Uthsara Basnayake
+ */
+public class SuperMarket {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //new ItemView().setVisible(true);
+        new CustomerView().setVisible(true);
+    }
+    
+}
